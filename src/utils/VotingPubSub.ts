73ffcfc4e5ce91c0e3b60fb0776044d0602206c7
Caio Gomes <caio.gomes@telavita.com.br>
2024-02-08 12:32:50 -0300

    Add VotingPubSub utility class

---
+++ src/utils/VotingPubSub.ts
@@ -0,0 +1,20 @@
+type Message = { pollOptionId: string, votes: number }
+
+class VotingPubSub {
+  private listeners: Record<string, ((message: Message) => void)[]> = {}
+  
+  subscribe(event: string, listener: (message: Message) => void) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = []
+    }
+    this.listeners[event].push(listener)
+  }
+
+  emit(event: string, message: Message) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach(listener => listener(message))
+    }
+  }
+}
+
+export const voting = new VotingPubSub()
