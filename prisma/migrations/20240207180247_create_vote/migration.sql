587866f47853ce5d3f77923cbbd7c56a91a098fd
Caio Gomes <caio.gomes@telavita.com.br>
2024-02-08 12:32:05 -0300

    Add database tables for Poll, PollOption, and Vote

---
+++ prisma/migrations/20240207180247_create_vote/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `votes` on the `PollOption` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "PollOption" DROP COLUMN "votes";
+
+-- CreateTable
+CREATE TABLE "Vote" (
+    "id" TEXT NOT NULL,
+    "sessionId" TEXT NOT NULL,
+    "pollOptionId" TEXT NOT NULL,
+    "pollId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Vote_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Vote_sessionId_pollId_key" ON "Vote"("sessionId", "pollId");
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_pollOptionId_fkey" FOREIGN KEY ("pollOptionId") REFERENCES "PollOption"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Vote" ADD CONSTRAINT "Vote_pollId_fkey" FOREIGN KEY ("pollId") REFERENCES "Poll"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
